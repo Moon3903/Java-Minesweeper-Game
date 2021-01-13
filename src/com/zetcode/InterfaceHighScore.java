84642e3b3aee3470601e28ef04241bae49b6d41a
zulfikar.fauzul.akbar <zulfikar.fauzul.akbar@gmail.com>
2021-01-13 11:51:31 +0700

    interface parent

---
+++ src/com/zetcode/InterfaceHighScore.java
@@ -0,0 +1,11 @@
+package com.zetcode;
+
+import java.nio.file.Path;
+
+public interface InterfaceHighScore {
+	public void compareHS(int baru);
+	public String getHS();
+	public Path checkFiles(Path path, Path file);
+	public String readFile();
+	public void writeFile(String str);
+}
